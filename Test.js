e759b4bb28afc01f5d4c6b0bcec5a710c98b15f4
Anupam Raj <anupam.r2301@gmail.com>
2024-06-23 14:18:18 +0530

    Test.js 1.0

--- Test.js
+++ Test.js
@@ -1,4 +1,5 @@
 function SwapNumbers(a,b){
+    console.log("This is the code of swap two numbers program")
     console.log (`The value of a : ${a} and b : ${b}`);
      [a,b] = [b,a];
      console.log (`The value of a : ${a} and b : ${b}`);
