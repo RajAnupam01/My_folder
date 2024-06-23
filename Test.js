b60d4fe7c400b38b6ff6450b295c7e00cc1ff165
Anupam Raj <133490478+RajAnupam01@users.noreply.github.com>
2024-06-23 14:35:24 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,5 +3,6 @@ function SwapNumbers(a,b){
     console.log (`The value of a : ${a} and b : ${b}`);
      [a,b] = [b,a];
      console.log (`The value of a : ${a} and b : ${b}`);
+    console.log("this is pull request demo");
 }
 SwapNumbers(5,10);
